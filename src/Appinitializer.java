9594fdf20de157ec5cee3569e7c8137c318d9cdd
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-03-01 13:02:30 +0530

    session added

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -1,5 +1,15 @@
+import lk.ijse.hibernate.enitity.Customer;
+
 public class Appinitializer {
     public static void main(String[] args) {
+        Customer c = new Customer();
+        c.setId("C001");
+        c.setName("Kamal");
+        c.setSalary(30000);
+        c.setAddress("Panadura");
+
+
+        session.save(c);
 
     }
 }
