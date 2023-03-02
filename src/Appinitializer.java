e29d0730f05ae31a10cae177f83ad968b313350f
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-03-02 10:58:44 +0530

    added getl method and update

--- src/Appinitializer.java
+++ src/Appinitializer.java
@@ -5,22 +5,27 @@ import org.hibernate.Transaction;
 
 public class Appinitializer {
     public static void main(String[] args) {
-        Customer c = new Customer("C001", "Gunapala", 56000, "Panadura");
-
-        Customer c1 = new Customer("C003", "Pasan", 20000, "Kaluthara");
 
         Customer c2 = new Customer("C004", "Malan", 70000, "Nagoda");
 
-        Customer c3 = new Customer("C005", "Gunadasa", 80000, "Nagoda");
+        //Customer c3 = new Customer("C005", "Gunadasa", 80000, "Nagoda");
 
-        Customer c4 = new Customer("C006", "Siriadasa", 80000, "Beruwala");
+        Customer c4 = new Customer("C006", "Pala", 80000, "Beruwala");
 
         Customer c5 = new Customer("C007", "Janaki", 80000, "Horana");
 
         Session session = Factoryconfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
-        session.save(c5);
+        // session.save(c4);
+
+        session.update(c4);
+
+
+        Customer c3=session.get(Customer.class,"C005");
+        System.out.println(c3);
+
+
         transaction.commit();
         session.close();
 
